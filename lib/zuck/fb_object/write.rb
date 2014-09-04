047b5e1f026244b43a390cadb7c3689db945fac8
Jonathan Bachman <jbachman@yotpo.com>
2014-09-04 10:10:19 +0300

    Zuck::FbObject::Write - .create support for objects that don't support the 'redownload' flag

--- lib/zuck/fb_object/write.rb
+++ lib/zuck/fb_object/write.rb
@@ -13,7 +13,7 @@ module Zuck
       data = data.stringify_keys
 
       # Don't post ids, because facebook doesn't like it
-      data = data.keep_if{ |k,v| k != "id" }
+      data = data.keep_if{ |k,v| k != 'id' }
 
       # Update on facebook
       result = post(graph, path, data)
@@ -23,11 +23,11 @@ module Zuck
       #     "campaigns" => { "12345" => "data" }
       #
       # Since we only put one at a time, we'll fetch this like that.
-      data = result["data"].values.first.values.first
+      data = result['data'].values.first.values.first
 
       # Update and return
       set_data(data)
-      result["result"]
+      result['result']
     end
 
     def destroy
@@ -47,17 +47,21 @@ module Zuck
         p = path || parent.path
 
         # We want facebook to return the data of the created object
-        data["redownload"] = 1
+        data['redownload'] = 1
 
         # Create
-        result = create_connection(graph, p, list_path, data)["data"]
+        result = create_connection(graph, p, list_path, data)
+        if result.has_key?('data')
+          # The data is nested by name and id, e.g.
+          #
+          #     "campaigns" => { "12345" => "data" }
+          #
+          # Since we only put one at a time, we'll fetch this like that.
+          data = result['data'].values.first.values.first
+        else
+          data = result
+        end
 
-        # The data is nested by name and id, e.g.
-        #
-        #     "campaigns" => { "12345" => "data" }
-        #
-        # Since we only put one at a time, we'll fetch this like that.
-        data = result.values.first.values.first
 
         # Return a new instance
         new(graph, data, parent)
